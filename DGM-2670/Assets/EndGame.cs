6a87e1e90dd158784b444e367eed53cad97036ba
Christian Gomez <10721401@my.uvu.edu>
2018-08-22 12:18:35 -0600

    fixing file issue

---
+++ DGM-2670/Assets/EndGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+
+	public FloutData Health;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Health.Value < 0)
+		{
+			//EndGame
+		}
+	}
+}
