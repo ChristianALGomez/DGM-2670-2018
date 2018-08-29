4979225b49136130687d1fb2fa955069dc48cb02
Christian Gomez <10721401@my.uvu.edu>
2018-08-29 10:59:48 -0600

    Event script test

---
+++ DGM-2670/Assets/Scripts/EVv2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EVv2 : MonoBehaviour {
+    
+    public UnityEvent Event;
+    public UnityEvent Stop;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Event.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Stop.Invoke();
+    }
+}
---
+++ DGM-2670/Assets/Scripts/NewEventScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NewEventScript : MonoBehaviour
+{
+
+    public UnityEvent MyEvent;
+
+    private void OnMouseDown()
+    {
+        MyEvent.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MyEvent.Invoke();
+    }
+}
+
