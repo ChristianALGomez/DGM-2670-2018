2a167fed34a439a6ba6861cc2c471676a8c8c808
Christian Gomez <10721401@my.uvu.edu>
2018-08-30 18:58:19 -0600

    Nine more Events
    Will list them out latter

---
+++ DGM-2670/Assets/EventBB/MouseOverEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseOverEvent : MonoBehaviour
+{
+
+    public UnityEvent Event;
+
+    private void OnMouseOver()
+    {
+        Event.Invoke();
+    }
+}
