e922515631b95d6d2d4ccd49a07ba4b181c5a686
Christian Gomez <10721401@my.uvu.edu>
2018-09-05 14:29:00 -0600

    characterControler Need to watch twitch

---
+++ DGM-2670/Assets/Floats/FloatDataScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatDataScript : ScriptableObject {
+    public float Value
+    {
+        get { return Value;}
+        set {}
+    }
+}
