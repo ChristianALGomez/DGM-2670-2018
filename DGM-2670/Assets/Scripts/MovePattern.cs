97e09f1cee5001e3d7ad073ddbb6011ad3f01d0f
Christian Gomez <10721401@my.uvu.edu>
2018-09-12 18:24:50 -0600

    Fly controls

--- DGM-2670/Assets/Scripts/MovePattern.cs
+++ DGM-2670/Assets/Scripts/MovePattern.cs
@@ -5,28 +5,35 @@ using UnityEngine;
 [CreateAssetMenu]
 public class MovePattern : ScriptableObject
 {
-    public FloatDataScript jumpSpeed;
     public FloatDataScript gravity;
 
     public FloatDataScript MoveX, MoveY, MoveZ;
     public FloatDataScript RotX, RotY, RotZ;
 	
-    private Vector3 moveDirection;
+    protected Vector3 moveDirection;
     private Vector3 rotDirection;
 
-    public void Invoke(CharacterController controller, Transform transform)
+    public virtual void Invoke(CharacterController controller, Transform transform)
     {
-        if (controller.isGrounded) {
-            moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-            rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
-            transform.Rotate(rotDirection);
-            moveDirection = transform.TransformDirection(moveDirection);
-			
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed.Value;
-            
+        if (controller.isGrounded)
+        {
+            Move(transform);
         }
+
+        Move(controller);
+    }
+
+    protected void Move(CharacterController controller)
+    {
         moveDirection.y -= gravity.Value * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    protected void Move(Transform transform)
+    {
+        moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+        rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+        transform.Rotate(rotDirection);
+        moveDirection = transform.TransformDirection(moveDirection);
     }
 }
