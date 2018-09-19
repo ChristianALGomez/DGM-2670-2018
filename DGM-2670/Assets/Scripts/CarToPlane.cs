99352044e41737d38a3f056f65f6c8420e070f2e
Christian Gomez <10721401@my.uvu.edu>
2018-09-19 16:36:13 -0600

    Need to make small game

---
+++ DGM-2670/Assets/Scripts/CarToPlane.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarToPlane : MovePattern
+{
+    private bool Switch;
+    public MovePattern car;
+    public MovePattern plane;
+    
+    public override void Invoke(CharacterController controller, Transform transform)
+    {
+        if (controller.isGrounded)
+        {
+            Move(transform);
+           Switch = true;
+        }
+        else
+        {
+            if (Switch)
+            {
+                Move(transform);
+                Switch = false;
+            }
+        }
+
+        Move(controller);
+    }
+}
