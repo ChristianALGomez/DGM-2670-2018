ff262a3238a3a2fe568e83bc5a2d99323041b97d
Christian Gomez <10721401@my.uvu.edu>
2018-09-20 22:43:58 -0600

    Movment camera

---
+++ DGM-2670/Assets/Scripts/CameraMovment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class CameraMovment : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 offset;
+	public Vector2 rot;
+	private void LateUpdate()
+	{
+		if (player != null)
+		{
+			transform.position = player.transform.position + offset;
+		}
+        
+	}
+}
