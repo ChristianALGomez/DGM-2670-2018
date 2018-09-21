c1b99809dad671561ae772980be3c9d55f2b60ac
Christian Gomez <10721401@my.uvu.edu>
2018-09-20 23:21:25 -0600

    Tweaks

--- DGM-2670/Assets/Scripts/AirTime.cs
+++ DGM-2670/Assets/Scripts/AirTime.cs
@@ -6,16 +6,18 @@ using UnityEngine.UI;
 public class AirTime : MonoBehaviour {
 
 	private Text label;
+	public MovePatternNotGrounded Air;
 
 	
     
 	IEnumerator Start()
-	{	
+	{
+		Air = GetComponent<MovePatternNotGrounded>();
 		label = GetComponent<Text>();
 
 		int number = 0;
 
-		while (number > 0)
+		while (Air)
 		{
 			yield return new WaitForSeconds(0.1f);
 			label.text = number.ToString();
