da550c92d6609a4bcd867b98b9b6aab3c8dc30b7
Christian Gomez <10721401@my.uvu.edu>
2018-10-09 14:37:59 -0600

    spawn falling dimands

---
+++ DGM-2670/Assets/Scripts/InfinateCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InfinateCounter : MonoBehaviour
+{
+    public UnityEvent Oncount;
+    public float Seconds = 1;
+
+
+
+        IEnumerator Start()
+        {
+            while (true)
+            {
+                Oncount.Invoke();
+                yield return new WaitForSeconds(Seconds);
+            }
+        }
+    }
