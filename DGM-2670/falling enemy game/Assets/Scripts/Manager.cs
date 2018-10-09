cfe873a9e68551b7aa9b0e48f0e60d72eca65fd6
Christian Gomez <10721401@my.uvu.edu>
2018-10-09 15:51:13 -0600

    Jump Game

---
+++ DGM-2670/falling enemy game/Assets/Scripts/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+
+    public static Manager instance = null;
+    public object currentScene;
+    public GameObject player;
+    void Awake() //singleton
+    {
+        //Singleton
+        if (instance == null)  //if instance is not assigned
+        {
+            instance = this;  //then assign instance to this object
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);  //then destroy this object
+        }
+    }
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  //load next scene going up by 1
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
