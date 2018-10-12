5e5f7d5eb8fc5eaf6ed5b5ba009d9a92b3dbcf2b
Christian Gomez <10721401@my.uvu.edu>
2018-10-12 16:54:59 -0600

    Enemy and Player have sepret move scripts

---
+++ DGM-2670/falling enemy game/Assets/Scripts/EnemeyMovePattern.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewMovment", menuName = "Move/EnemyMove")]
+public class EnemeyMovePattern : ScriptableObject {
+	
+	public FloatDataScript gravity;
+	public FloatDataScript MoveX, MoveY, MoveZ;
+	public FloatDataScript RotX, RotY, RotZ;
+	
+	protected Vector3 moveDirection;
+	private Vector3 rotDirection;
+	public bool enabled;
+
+	public virtual void Invoke(CharacterController controller, Transform transform)
+	{
+		if (controller.isGrounded)
+		{
+			Move(transform);
+		}
+
+		Move(controller);
+	}
+
+	protected void Move(CharacterController controller)
+	{
+		moveDirection.y -= gravity.Value;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	protected void Move(Transform transform)
+	{
+		moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+		rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+		transform.Rotate(rotDirection);
+		moveDirection = transform.TransformDirection(moveDirection);
+	}
+}
---
+++ DGM-2670/falling enemy game/Assets/Scripts/EnemyMovment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovment : MonoBehaviour {
+	
+	
+	private CharacterController controller;
+	public EnemeyMovePattern EMove;
+	
+
+	private void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+    
+	void Update() {
+		EMove.Invoke(controller, transform);
+	}
+}
+
