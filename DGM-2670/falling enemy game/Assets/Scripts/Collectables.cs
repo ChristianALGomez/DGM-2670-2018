132150ce1ef3390f872f6793f9b141cbb9c77e24
Christian Gomez <10721401@my.uvu.edu>
2018-10-14 23:16:29 -0600

    Time Adjustment need to add scoring system

---
+++ DGM-2670/falling enemy game/Assets/Scripts/Collectables.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour {
+
+    public int value;
+    public Color colorTint;
+    private GameObject Holder;
+    public Holder myHolder;
+
+    private void Start()
+    {
+        myHolder = FindObjectOfType<Holder>().GetComponent<Holder>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        myHolder.IncrementScore(value);
+        Destroy(gameObject);
+    }
+}
