750d673d161f9134a3febdacc8031b25a358bff4
Christian Gomez <10721401@my.uvu.edu>
2018-10-15 18:20:30 -0600

    Enemy dies/Point Need to get it to work only for ground

---
+++ DGM-2670/falling enemy game/Assets/Scripts/Points.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour {
+
+	public int value;
+	public Color colorTint;
+	private GameObject Holder;
+	public Holder myHolder;
+	public GameObject Thing;
+
+	private void Start()
+	{
+		myHolder = FindObjectOfType<Holder>().GetComponent<Holder>();
+	}
+
+	/*private void OnTriggerEnter(Collider collider)
+	{
+		if (Object)
+		{
+			myHolder.IncrementScore(value);
+			Destroy(gameObject);
+		}
+	}*/
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		if (Thing)
+		{
+			myHolder.IncrementScore(value);
+			Destroy(gameObject);
+		}
+	}
+}
