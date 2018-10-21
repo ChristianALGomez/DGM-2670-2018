4b57fb8d1d878f80ad6e6774d0a1c88e6314e69c
Christian Gomez <10721401@my.uvu.edu>
2018-10-20 19:28:53 -0600

    Trying to get game over scene to work

---
+++ DGM-2670/falling enemy game/Assets/Scripts/Floor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+
+    public Manager theGameManger;
+    public Holder EndTime;
+
+    private void Start()
+    {
+        theGameManger = GameObject.Find("GameManager").GetComponent<Manager>();
+        EndTime = GameObject.Find("Holder").GetComponent<Holder>();
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        print("GameOver!!");
+        //theGameManger.LoadLevel("GameOver");
+        theGameManger.HP.value--;
+        Destroy(collision.gameObject);
+
+        if (theGameManger.HP.value <= 0)
+        {
+            theGameManger.LoadLevel("GameOver");
+            EndTime.Finished();
+
+        }
+        if (theGameManger.HP.value > 0)
+        {
+            theGameManger.Respawn();
+        }
+        theGameManger.ChangeLD();
+    }*/
+}
--- DGM-2670/falling enemy game/Assets/Scripts/KillPlayer.cs
+++ DGM-2670/falling enemy game/Assets/Scripts/KillPlayer.cs
@@ -18,4 +18,5 @@ public class KillPlayer : MonoBehaviour
             Destroy(player);
         }
     }
+    
 }
