09d04e303314e16da3df99929224c0d79d813501
Christian Gomez <10721401@my.uvu.edu>
2018-11-05 16:42:16 -0700

    Need to get tokens to instantiate shield

---
+++ DGM-2670/falling enemy game/Assets/Scripts/ShieldSpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldSpawn : MonoBehaviour
+{
+
+    public GameObject Sheild;
+    
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Token") == collider)
+        {
+            Instantiate(Sheild);
+        }
+    }
+}
