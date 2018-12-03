975e95a4ef3d6baf35aca0c6b5ce89d48355e00c
Christian Gomez <10721401@my.uvu.edu>
2018-12-03 10:51:33 -0700

    store scripts in unity

---
+++ DGM-2670/falling enemy game/Assets/Scripts/Store/PurchasableObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Purchase", menuName = "Store/Purchasable")]
+public class PurchasableObject : ScriptableObject
+{
+	public Object Item;
+	public Sprite PreviewArt;
+	public int UsageCount = 3;
+	public int Value;
+	public bool Upgrade;
+	public PurchasableObject UpgradeFrom;
+
+	public bool Perpetual;
+	public bool Instanciatable;
+
+	public void CreateItem()
+	{
+		for (int i = 0; i < UsageCount; i++)
+		{
+			if (Instanciatable && Item is GameObject)
+			{
+				Instantiate(Item);
+			}
+		}
+	}
+
+	public void Used()
+	{
+		UsageCount--;
+	}
+}
---
+++ DGM-2670/falling enemy game/Assets/Scripts/Store/PurchaseableObjects.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Objects", menuName = "Store/Objects")]
+public class PurchaseableObjects : ScriptableObject 
+{
+	public List<PurchasableObject> ObjectList;
+}
